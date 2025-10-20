3ab81cb0c523284f6d873bf0a942367b1121ae69
ra870860 <rafarealeisv9@gmail.com>
2025-10-20 14:44:06 +0200

    Completa

--- src/main/java/com/example/wordly/HelloController.java
+++ src/main/java/com/example/wordly/HelloController.java
@@ -1,19 +1,21 @@
 package com.example.wordly;
 
-import javafx.fxml.FXML;
+import javafx.fxml.FXML;//importo el FXML
 import javafx.scene.control.Label;
-import javafx.scene.control.Button;
-import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
+import javafx.scene.control.Button;//importo los botones
+import javafx.event.ActionEvent;//importo el evento de click etc.
+import javafx.scene.control.Alert;//importo la alerta
+import javafx.scene.control.ButtonType;//importo los botones para la alerta
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.Arrays;//importamos para el array que necesita la lista
+import java.util.List;//importamos para usar la lista
+import java.util.Optional;//importamas para la alerta el famoso error NullPointerException
+import java.util.Random;//importamos para coger aleatoriamente una palabra
 
 import java.awt.*;
 
 public class HelloController {
+    //<-- importamos los botones de FXML en java -->
     @FXML
     private Button mostrarBoton00;
     @FXML
@@ -75,13 +77,18 @@ public class HelloController {
     @FXML
     private Button mostrarBoton29;
 
+    //<-- variable globales -->
+    //tipo Lista <el tipo de la lista>
     private List<Button> panelBotones;
     private int filaActual = 0;
     private int columnaActual = 0;
     private final int MAX_COLUMNAS = 5;
     private final int MAX_FILAS = 6;
+    private String palabraSecreta;
+    private List<String> diccionario = Arrays.asList("actas", "aguas", "aires", "ajena", "almas", "altar", "autos", "aulas", "avión", "barra", "barco", "bases", "bello", "bocas", "bolso", "botón", "cabra", "calle", "campo", "canta", "carta", "casas", "causa", "cebra", "cejas", "celda", "cenar", "cerca", "claro", "clase", "clave", "coche", "comer", "copas", "corro", "corto", "costa", "crema", "cruce", "culpa", "curar", "datos", "deber", "dedos", "dicha", "diez", "dinos", "dolor", "donde", "dueño", "entró", "estar", "fecha", "feliz", "final", "fondo", "forma", "frase", "fuego", "gente", "grito", "grupo", "hacer", "hojas", "hotel", "ideas", "jugar", "junto", "largo", "lejos", "libra", "libros", "línea", "lugar", "lunes", "males", "manos", "marco", "meter", "miedo", "monte", "moral", "motor", "mujer", "nacer", "noche", "norte", "nueva", "nubes", "ondas", "orden", "otros", "padre", "palma", "papel", "parte", "pasos", "pausa", "peces", "perro");
+    private Random random = new Random();
 
-    @FXML
+    // metemos todos los botones en una lista
     public void initialize() {
         panelBotones = Arrays.asList(mostrarBoton00, mostrarBoton01, mostrarBoton02, mostrarBoton03, mostrarBoton04,
                 mostrarBoton05, mostrarBoton06, mostrarBoton07, mostrarBoton08, mostrarBoton09, mostrarBoton10,
@@ -89,21 +96,30 @@ public class HelloController {
                 mostrarBoton17, mostrarBoton18, mostrarBoton19, mostrarBoton20, mostrarBoton21, mostrarBoton22,
                 mostrarBoton23, mostrarBoton24, mostrarBoton25, mostrarBoton26, mostrarBoton27, mostrarBoton28,
                 mostrarBoton29);
-        System.out.println();
+        seleccionarPalabraSecreta();
     }
 
+    //funcion para cojer una palabra secreta aleatoria
+    private void seleccionarPalabraSecreta() {
+        //.get cojemos algo de diccionario (random.nextInt(aqui va la cantidad de elementos aleatorios que vamos a randomizar))
+        palabraSecreta = diccionario.get(random.nextInt(diccionario.size()));
+        System.out.println("Palabra secreta: " + palabraSecreta);
+    }
+
+    //funcion para la alerta de fin de partida o gameOver
     private void finJuego() {
+        //objeto tipo alerta , nueva alerta (alerta.indicamos alertType.tipo de alerta
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Fin del juego");
-        alert.setHeaderText("¡Has agotado todos tus intentos!");
-        alert.setContentText("¿Quieres jugar de nuevo?");
+        alert.setTitle("Fin del juego");//titulo
+        alert.setHeaderText("¡Has agotado todos tus intentos!");//cabecera
+        alert.setContentText("¿Quieres jugar de nuevo?");//cuerpo
 
-        ButtonType botonReiniciar = new ButtonType("Reiniciar");
-        ButtonType botonSalir = new ButtonType("Salir");
+        ButtonType botonReiniciar = new ButtonType("Reiniciar");//un boton para reiniciar nuevo ButtonType(texto)
+        ButtonType botonSalir = new ButtonType("Salir");//lo mismo pero para salir
 
-        alert.getButtonTypes().setAll(botonReiniciar, botonSalir);
+        alert.getButtonTypes().setAll(botonReiniciar, botonSalir);//llamamos a los botnes .gettButtonTypes() los sustituimos por los parametros .setAll(parametro1, parametro2)
 
-        Optional<ButtonType> result = alert.showAndWait();
+        Optional<ButtonType> result = alert.showAndWait();// para que no de un valor null que blockea el codigo,
         if (result.isPresent() && result.get() == botonReiniciar) {
             reiniciarJuego();
         } else {
@@ -116,14 +132,16 @@ public class HelloController {
         columnaActual = 0;
         for (Button boton : panelBotones) {
             boton.setText(" ");
-            // para añadir stylos mas adelante: boton.setStyle("-fx-background-color: orange;");
+            boton.setStyle(" ");
         }
+        seleccionarPalabraSecreta();
     }
 
     @FXML
     protected void buttonClick(ActionEvent event) {
         Button boton = (Button) event.getSource();
         String letra = boton.getText();
+
         if (letra.equals("DELETE")) {
             if (columnaActual > 0) {
                 columnaActual--;
@@ -132,10 +150,15 @@ public class HelloController {
             }
         } else if (letra.equals("ENTER")){
             if (columnaActual == MAX_COLUMNAS) {
-                filaActual++;
-                columnaActual = 0;
-                if (filaActual >= MAX_FILAS) {
-                    finJuego();
+                boolean juegoGanado = comprobarPalabra();
+                if (!juegoGanado) {
+                    if (filaActual<MAX_FILAS) {
+                        filaActual++;
+                        columnaActual = 0;
+                    }
+                    if (filaActual >= MAX_FILAS) {
+                        finJuego();
+                    }
                 }
             }
         } else {
@@ -145,5 +168,42 @@ public class HelloController {
                 columnaActual++;
             }
         }
+    }
+    private boolean comprobarPalabra() {
+        StringBuilder palabraAdivinada = new StringBuilder();
+        for (int i = 0; i < MAX_COLUMNAS; i++) {
+            int index = filaActual * MAX_COLUMNAS + i;
+            palabraAdivinada.append(panelBotones.get(index).getText());
+        }
+
+        String palabra = palabraAdivinada.toString().toUpperCase();
+        String palabraSecretaMayusculas = palabraSecreta.toUpperCase();
+        boolean aciertoCompleto = true;
+
+        for (int i = 0; i < MAX_COLUMNAS; i++) {
+            Button boton = panelBotones.get(filaActual * MAX_COLUMNAS + i);
+            String letra = String.valueOf(palabra.charAt(i));
+
+            if (palabraSecretaMayusculas.charAt(i) == letra.charAt(0)) {
+                boton.setStyle("-fx-background-color: green;");
+            } else if (palabraSecretaMayusculas.contains(letra)) {
+                boton.setStyle("-fx-background-color: yellow;");
+                aciertoCompleto = false;
+            }else {
+                boton.setStyle("-fx-background-color: gray;");
+                aciertoCompleto = false;
+            }
+        }
+
+        if (aciertoCompleto) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("¡Felicidades!");
+            alert.setHeaderText("Has acertado la palabra secreta!");
+            alert.setContentText("La palabra era: " + palabraSecreta);
+            alert.showAndWait();
+            reiniciarJuego();
+            return true;
+        }
+        return false;
     }
 }
